36d29d7c5d883dc71388de76601bf3451ab03767
swcc cwcw <nickorici@yahoo.com>
2019-04-10 01:35:33 +0300

    Add project files.

---
+++ Components/ChatStream.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Components
+{
+    public class ChatStream
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
